c8afee8d5bf9f800e2bbcfc8b7f5b36bbb75fd16
Juraci Vieira <juraci.vieira@gmail.com>
2019-01-08 12:40:01 -0200

    basic VueX setup

--- app/main.js
+++ app/main.js
@@ -1,9 +1,48 @@
+const state = {
+  notes: [],
+  timestamps: [],
+}
+
+const mutations = {
+  ADD_NOTE (state, payload) {
+    let newNote = payload;
+    state.notes.push(newNote);
+  },
+  ADD_TIMESTAMP (state, payload) {
+    let newTimeStamp = payload;
+    state.timestamps.push(newTimeStamp);
+  },
+}
+
+const actions = {
+  addNote (context, payload) {
+    context.commit('ADD_NOTE', payload);
+  },
+  addTimestamp (context, payload) {
+    context.commit('ADD_TIMESTAMP', payload);
+  },
+}
+
+const getters = {
+  getterNotes: state => state.notes,
+  getterTimeStamps: state => state.timestamps,
+  getterNoteCount: state => state.notes.length,
+}
+
+const store = new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters,
+})
+
 const inputComponent = {
   template: `<input placeholder='Enter a note' class="input is-small" type="text" />`,
 }
 
 new Vue({
   el: '#app',
+  store,
   components: {
     'input-component': inputComponent
   }
